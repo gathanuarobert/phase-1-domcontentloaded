e61041a7c90c9143d4696ebdeb1ac1bcd90d167b
Robert Gathanua <robertgathanua@gamail.com>
2024-03-28 16:24:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const id = document.getElementById('text')
+
+    id.textContent = "This is really cool!"
+})
+
